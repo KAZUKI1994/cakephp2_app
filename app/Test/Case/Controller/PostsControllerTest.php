d337f9f78ba5d948f9c093c0a7302382541a93e6
kazuku higashiguchi <higashiguchi_kazuki@yahoo.co.jp>
2017-08-25 12:58:32 +0900

    xdebugのinstall & fixtureさくせいｖ

---
+++ app/Test/Case/Controller/PostsControllerTest.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: higashiguchi0kazuki
+ * Date: 8/25/17
+ * Time: 12:41
+ */
+
+/**
+ * PostsController Test Case
+ */
+class PostsControllerTest extends ControllerTestCase{
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = array(
+        'app/posts'
+    );
+
+    /**
+     * testSearch method
+     */
+    public function testSearch(){
+
+    }
+
+
+}
---
+++ app/Test/Fixture/PostFixture.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Post Fixture
+ */
+class PostFixture extends CakeTestFixture {
+
+/**
+ * Import
+ *
+ * @var array
+ */
+	public $import = array('model' => 'Post', 'records' => true);
+
+    // ToDo: テストレコードの指定
+}
